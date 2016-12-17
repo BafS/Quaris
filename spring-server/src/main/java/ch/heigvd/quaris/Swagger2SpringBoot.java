8f4c607e28485c2d5c8d49695864235eaa4470dd
BafS <fabacrans@gmail.com>
2016-12-17 17:15:10 +0100

    Use devtools

--- spring-server/src/main/java/ch/heigvd/quaris/Swagger2SpringBoot.java
+++ spring-server/src/main/java/ch/heigvd/quaris/Swagger2SpringBoot.java
@@ -3,6 +3,7 @@ package ch.heigvd.quaris;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.context.annotation.ComponentScan;
@@ -10,7 +11,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-@ComponentScan(basePackages = "ch.heigvd.quaris")
+@ComponentScan
 public class Swagger2SpringBoot implements CommandLineRunner {
 
 	@Override
@@ -20,7 +21,7 @@ public class Swagger2SpringBoot implements CommandLineRunner {
 		}
 	}
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String... args) throws Exception {
 		new SpringApplication(Swagger2SpringBoot.class).run(args);
 	}
 
